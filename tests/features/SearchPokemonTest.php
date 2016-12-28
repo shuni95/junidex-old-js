c2b8c3b23926b949967b1d4a5ffd6ded60b72607
Junior Claudio <junior.claudio@tektonlabs.com>
2016-12-28 16:48:53 +0000

    variables unused

--- tests/features/SearchPokemonTest.php
+++ tests/features/SearchPokemonTest.php
@@ -13,9 +13,9 @@ class SearchPokemonTest extends TestCase
     /** @test */
     public function user_can_view_pokemon_in_the_results_using_part_of_the_name()
     {
-        $pokemonA = factory(Pokemon::class)->create(['name' => 'Charmander']);
-        $pokemonB = factory(Pokemon::class)->create(['name' => 'Charizard']);
-        $pokemonC = factory(Pokemon::class)->create(['name' => 'Psyduck']);
+        factory(Pokemon::class)->create(['name' => 'Charmander']);
+        factory(Pokemon::class)->create(['name' => 'Charizard']);
+        factory(Pokemon::class)->create(['name' => 'Psyduck']);
 
         $this->visit('/searchPokemon?name=Char')
              ->see('Charmander')
@@ -31,9 +31,9 @@ class SearchPokemonTest extends TestCase
     /** @test */
     public function user_can_view_pokemon_in_the_results_using_part_of_the_name_in_lowercase()
     {
-        $pokemonA = factory(Pokemon::class)->create(['name' => 'Charmander']);
-        $pokemonB = factory(Pokemon::class)->create(['name' => 'Charizard']);
-        $pokemonC = factory(Pokemon::class)->create(['name' => 'Psyduck']);
+        factory(Pokemon::class)->create(['name' => 'Charmander']);
+        factory(Pokemon::class)->create(['name' => 'Charizard']);
+        factory(Pokemon::class)->create(['name' => 'Psyduck']);
 
         $this->visit('/searchPokemon?name=char')
              ->see('Charmander')
@@ -49,9 +49,9 @@ class SearchPokemonTest extends TestCase
     /** @test */
     public function user_can_view_pokemon_in_the_results_using_part_of_the_name_in_uppercase()
     {
-        $pokemonA = factory(Pokemon::class)->create(['name' => 'Charmander']);
-        $pokemonB = factory(Pokemon::class)->create(['name' => 'Charizard']);
-        $pokemonC = factory(Pokemon::class)->create(['name' => 'Psyduck']);
+        factory(Pokemon::class)->create(['name' => 'Charmander']);
+        factory(Pokemon::class)->create(['name' => 'Charizard']);
+        factory(Pokemon::class)->create(['name' => 'Psyduck']);
 
         $this->visit('/searchPokemon?name=CHAR')
              ->see('Charmander')
@@ -67,9 +67,9 @@ class SearchPokemonTest extends TestCase
     /** @test */
     public function user_can_view_pokemon_in_the_results_using_part_of_the_japanese_name()
     {
-        $pokemonA = factory(Pokemon::class)->create(['japanese_name' => 'Hitokage']);
-        $pokemonB = factory(Pokemon::class)->create(['japanese_name' => 'Lizardon']);
-        $pokemonC = factory(Pokemon::class)->create(['japanese_name' => 'Koduck']);
+        factory(Pokemon::class)->create(['japanese_name' => 'Hitokage']);
+        factory(Pokemon::class)->create(['japanese_name' => 'Lizardon']);
+        factory(Pokemon::class)->create(['japanese_name' => 'Koduck']);
 
         $this->visit('/searchPokemon?name=Hito')
              ->see('Hitokage')
@@ -87,9 +87,9 @@ class SearchPokemonTest extends TestCase
     /** @test */
     public function user_can_view_pokemon_in_the_results_using_part_of_the_japanese_katakana()
     {
-        $pokemonA = factory(Pokemon::class)->create(['japanese_katakana' => 'ヒトカゲ']);
-        $pokemonB = factory(Pokemon::class)->create(['japanese_katakana' => 'リザードン']);
-        $pokemonC = factory(Pokemon::class)->create(['japanese_katakana' => 'コダック']);
+        factory(Pokemon::class)->create(['japanese_katakana' => 'ヒトカゲ']);
+        factory(Pokemon::class)->create(['japanese_katakana' => 'リザードン']);
+        factory(Pokemon::class)->create(['japanese_katakana' => 'コダック']);
 
         $this->visit('/searchPokemon?name=リザー')
              ->see('リザードン')
@@ -99,11 +99,11 @@ class SearchPokemonTest extends TestCase
     /** @test */
     public function user_can_view_pokemon_in_the_results_using_the_pokemon_type()
     {
-        $pokemonA = factory(Pokemon::class)->create(['name' => 'Charmander', 'type_one' => 'Fire']);
-        $pokemonB = factory(Pokemon::class)->create(['name' => 'Charizard', 'type_one' => 'Fire','type_two' => 'Flying']);
-        $pokemonC = factory(Pokemon::class)->create(['name' => 'Psyduck', 'type_one' => 'Water']);
-        $pokemonD = factory(Pokemon::class)->create(['name' => 'Squirtle', 'type_one' => 'Water']);
-        $pokemonE = factory(Pokemon::class)->create(['name' => 'Arceus', 'type_one' => 'Normal']);
+        factory(Pokemon::class)->create(['name' => 'Charmander', 'type_one' => 'Fire']);
+        factory(Pokemon::class)->create(['name' => 'Charizard', 'type_one' => 'Fire','type_two' => 'Flying']);
+        factory(Pokemon::class)->create(['name' => 'Psyduck', 'type_one' => 'Water']);
+        factory(Pokemon::class)->create(['name' => 'Squirtle', 'type_one' => 'Water']);
+        factory(Pokemon::class)->create(['name' => 'Arceus', 'type_one' => 'Normal']);
 
         $this->visit('/searchPokemon?type=Fire')
              ->see('Charmander')
