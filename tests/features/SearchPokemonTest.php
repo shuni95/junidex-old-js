4513bb69f560fc58f38c549b3a0df107a2b0f39b
Junior Claudio <junior.claudio@tektonlabs.com>
2016-12-28 13:40:54 +0000

    more test with the pokemon name

--- tests/features/SearchPokemonTest.php
+++ tests/features/SearchPokemonTest.php
@@ -11,19 +11,57 @@ class SearchPokemonTest extends TestCase
     use DatabaseMigrations;
 
     /** @test */
-    public function user_can_view_some_pokemon_in_the_search_results()
+    public function user_can_view_pokemon_in_the_results_using_part_of_the_name()
     {
-        // Data
         $pokemonA = Pokemon::create(['name' => 'Charmander',]);
         $pokemonB = Pokemon::create(['name' => 'Charizard',]);
         $pokemonC = Pokemon::create(['name' => 'Psyduck',]);
 
-        // Process
         $this->visit('/searchPokemon?name=Char');
-
-        // Validar
         $this->see('Charmander');
         $this->see('Charizard');
         $this->dontSee('Psyduck');
+
+        $this->visit('/searchPokemon?name=Psy');
+        $this->dontSee('Charmander');
+        $this->dontSee('Charizard');
+        $this->see('Psyduck');
     }
+
+    /** @test */
+    public function user_can_view_pokemon_in_the_results_using_part_of_the_name_in_lowercase()
+    {
+        $pokemonA = Pokemon::create(['name' => 'Charmander',]);
+        $pokemonB = Pokemon::create(['name' => 'Charizard',]);
+        $pokemonC = Pokemon::create(['name' => 'Psyduck',]);
+
+        $this->visit('/searchPokemon?name=char');
+        $this->see('Charmander');
+        $this->see('Charizard');
+        $this->dontSee('Psyduck');
+
+        $this->visit('/searchPokemon?name=psy');
+        $this->dontSee('Charmander');
+        $this->dontSee('Charizard');
+        $this->see('Psyduck');
+    }
+
+    /** @test */
+    public function user_can_view_pokemon_in_the_results_using_part_of_the_name_in_uppercase()
+    {
+        $pokemonA = Pokemon::create(['name' => 'Charmander',]);
+        $pokemonB = Pokemon::create(['name' => 'Charizard',]);
+        $pokemonC = Pokemon::create(['name' => 'Psyduck',]);
+
+        $this->visit('/searchPokemon?name=CHAR');
+        $this->see('Charmander');
+        $this->see('Charizard');
+        $this->dontSee('Psyduck');
+
+        $this->visit('/searchPokemon?name=PSY');
+        $this->dontSee('Charmander');
+        $this->dontSee('Charizard');
+        $this->see('Psyduck');
+    }
+
 }
