eb788b10686da58b571273508bd817875809f926
Junior Claudio <junior.claudio@tektonlabs.com>
2016-12-29 19:05:58 +0000

    test evolutions of pokemon that evolves with level and megastone

--- tests/features/ViewEvolutionChainTest.php
+++ tests/features/ViewEvolutionChainTest.php
@@ -22,18 +22,8 @@ class ViewEvolutionChainTest extends TestCase
 
         $method = EvolutionMethod::create(['id' => EvolutionaryMethodConstants::LEVEL_METHOD, 'name' => 'by Level']);
 
-        $caterpie_to_metapod = Evolution::create([
-            'pokemon_id' => $caterpie->id,
-            'evolution_id' => $metapod->id,
-            'method_id' => $method->id,
-            'details' => 'lvl 7'
-        ]);
-        $metapod_to_butterfree = Evolution::create([
-            'pokemon_id' => $metapod->id,
-            'evolution_id' => $butterfree->id,
-            'method_id' => $method->id,
-            'details' => 'lvl 10'
-        ]);
+        Evolution::create(['pokemon_id' => $caterpie->id, 'evolution_id' => $metapod->id, 'method_id' => $method->id, 'details' => 'lvl 7']);
+        Evolution::create(['pokemon_id' => $metapod->id, 'evolution_id' => $butterfree->id, 'method_id' => $method->id, 'details' => 'lvl 10']);
 
         $this->visit('/evolution_chain/Butterfree')
              ->see('Caterpie evolves into Metapod at lvl 7')
@@ -48,7 +38,7 @@ class ViewEvolutionChainTest extends TestCase
 
         $method = EvolutionMethod::create(['id' => EvolutionaryMethodConstants::EVOLUTIONARY_STONE_METHOD, 'name' => 'by evolutionary stone']);
 
-        $growlithe_to_arcanine = Evolution::create([
+        Evolution::create([
             'pokemon_id' => $growlithe->id,
             'evolution_id' => $arcanine->id,
             'method_id' => $method->id,
@@ -57,5 +47,29 @@ class ViewEvolutionChainTest extends TestCase
 
         $this->visit('/evolution_chain/Arcanine')
              ->see('Growlithe evolves into Arcanine when exposed to a Fire Stone');
+    }
+
+    /** @test */
+    public function user_can_view_evolutions_of_pokemon_that_evolves_with_level_and_megastone()
+    {
+        $charmander = factory(Pokemon::class)->create(['name' => 'Charmander']);
+        $charmeleon = factory(Pokemon::class)->create(['name' => 'Charmeleon']);
+        $charizard = factory(Pokemon::class)->create(['name' => 'Charizard']);
+        $mega_charizard_x = factory(Pokemon::class)->create(['name' => 'Mega Charizard X']);
+        $mega_charizard_y = factory(Pokemon::class)->create(['name' => 'Mega Charizard Y']);
+
+        $level_method = EvolutionMethod::create(['id' => EvolutionaryMethodConstants::LEVEL_METHOD, 'name' => 'by Level']);
+        $mega_stone_method = EvolutionMethod::create(['id' => EvolutionaryMethodConstants::MEGASTONE_METHOD, 'name' => 'using']);
+
+        Evolution::create(['pokemon_id' => $charmander->id, 'evolution_id' => $charmeleon->id, 'method_id' => $level_method->id, 'details' => 'lvl 16']);
+        Evolution::create(['pokemon_id' => $charmeleon->id, 'evolution_id' => $charizard->id, 'method_id' => $level_method->id, 'details' => 'lvl 36']);
+        Evolution::create(['pokemon_id' => $charizard->id, 'evolution_id' => $mega_charizard_x->id, 'method_id' => $level_method->id, 'details' => 'Charizardite X']);
+        Evolution::create(['pokemon_id' => $charizard->id, 'evolution_id' => $mega_charizard_y->id, 'method_id' => $level_method->id, 'details' => 'Charizardite Y']);
+
+        $this->visit('/evolution_chain/Charizard')
+             ->see('Charmander evolves into Charmeleon at lvl 16')
+             ->see('Charmeleon evolves into Charizard at lvl 16')
+             ->see('Charizard evolves into Mega Charizard X using Charizardite X')
+             ->see('Charizard evolves into Mega Charizard Y using Charizardite Y');
     }
 }
