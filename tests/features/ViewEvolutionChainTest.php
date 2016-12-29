738025a1a9d86a7d026a4e980b7bf3c4a8f7982b
Junior Claudio <junior.claudio@tektonlabs.com>
2016-12-29 13:52:58 +0000

    reformulate again

--- tests/features/ViewEvolutionChainTest.php
+++ tests/features/ViewEvolutionChainTest.php
@@ -5,6 +5,8 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 use App\Pokemon;
+use App\EvolutionMethod;
+use App\Evolution;
 
 class ViewEvolutionChainTest extends TestCase
 {
@@ -17,22 +19,32 @@ class ViewEvolutionChainTest extends TestCase
         $metapod = factory(Pokemon::class)->create(['name' => 'Metapod']);
         $butterfree = factory(Pokemon::class)->create(['name' => 'Butterfree']);
 
-        $method = factory(EvolutionMethod::class)->create(['name' => 'by Level']);
+        $method = EvolutionMethod::create(['name' => 'by Level']);
 
-        $caterpie_to_metapod = factory(Evolution::class)->create(['pokemon_id' => $caterpie->id, 'evolution_id' => $metapod->id, 'method' => $method->id, 'lvl 7']);
-        $metapod_to_butterfree = factory(Evolution::class)->create(['pokemon_id' => $metapod->id, 'evolution_id' => $butterfree->id, 'method' => $method->id, 'lvl 10']);
+        $caterpie_to_metapod = Evolution::create([
+            'pokemon_id' => $caterpie->id,
+            'evolution_id' => $metapod->id,
+            'method_id' => $method->id,
+            'details' => 'lvl 7'
+        ]);
+        $metapod_to_butterfree = Evolution::create([
+            'pokemon_id' => $metapod->id,
+            'evolution_id' => $butterfree->id,
+            'method_id' => $method->id,
+            'details' => 'lvl 10'
+        ]);
 
-        $this->visit('/evolution_chain?pokemon=Caterpie')
+        $this->visit('/evolution_chain/Caterpie')
              ->see('Caterpie')
              ->see('Metapod')
              ->see('Butterfree');
 
-        $this->visit('/evolution_chain?pokemon=Metapod')
+        $this->visit('/evolution_chain/Metapod')
              ->see('Caterpie')
              ->see('Metapod')
              ->see('Butterfree');
 
-        $this->visit('/evolution_chain?pokemon=Butterfree')
+        $this->visit('/evolution_chain/Butterfree')
              ->see('Caterpie')
              ->see('Metapod')
              ->see('Butterfree');
