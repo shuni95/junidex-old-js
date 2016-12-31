b597fd60be2b8b28df36d2d97159c252eb0ee618
Junior Claudio <juniorclaudiozavaleta@gmail.com>
2016-12-31 17:39:31 +0000

    redirect when try access direct to thanks view

--- app/Http/Controllers/TrainerRegisterController.php
+++ app/Http/Controllers/TrainerRegisterController.php
@@ -29,11 +29,15 @@ class TrainerRegisterController extends Controller
             'user_id' => $user->id
         ]);
 
-        return redirect()->route('app.trainers.register.thanks');
+        return redirect()->route('app.trainers.register.thanks')->with('thanks', true);
     }
 
     public function thanks()
     {
+        if (!session('thanks')) {
+            return redirect()->route('app.trainers.register.showForm')->with(['error_message' => 'You must register before']);
+        }
+
         return view('app.trainers.thanks_for_register');
     }
 }
