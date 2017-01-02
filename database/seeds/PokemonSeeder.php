5a430cc9cea7c3280648ea2dbb0f36c0a2605be4
Junior Claudio <juniorclaudiozavaleta@gmail.com>
2017-01-02 20:14:50 +0000

    pokemon seeder

---
+++ database/seeds/PokemonSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Pokemon;
+
+class PokemonSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Pokemon::class)->create(['name' => 'Charmander', 'japanese_name' => 'Hitokage', 'japanese_katakana' => 'ヒトカゲ', 'type_one' => 'Fire', 'habitat' => 'Mountain']);
+        factory(Pokemon::class)->create(['name' => 'Charizard', 'japanese_name' => 'Lizardon', 'japanese_katakana' => 'リザードン', 'type_one' => 'Fire','type_two' => 'Flying', 'habitat' => 'Mountain']);
+        factory(Pokemon::class)->create(['name' => 'Psyduck', 'japanese_name' => 'Koduck', 'japanese_katakana' => 'コダック', 'type_one' => 'Water', 'habitat' => 'Fresh Water']);
+        factory(Pokemon::class)->create(['name' => 'Squirtle', 'type_one' => 'Water', 'habitat' => 'Fresh Water']);
+        factory(Pokemon::class)->create(['name' => 'Arceus', 'type_one' => 'Normal', 'habitat' => 'Not determined']);
+        factory(Pokemon::class)->create(['name' => 'Pikachu', 'habitat' => 'Forest']);
+        factory(Pokemon::class)->create(['name' => 'Metapod', 'habitat' => 'Forest']);
+        factory(Pokemon::class)->create(['name' => 'Dratini', 'habitat' => 'Fresh Water']);
+        factory(Pokemon::class)->create(['name' => 'Arcanine', 'type_one' => 'Fire', 'habitat' => 'Meadow']);
+        factory(Pokemon::class)->create(['name' => 'Lapras',     'type_one' => 'Water', 'habitat' => 'Salt Water', 'type_two' => 'Ice']);
+    }
+}
--- tests/features/SearchPokemonTest.php
+++ tests/features/SearchPokemonTest.php
@@ -14,10 +14,6 @@ class SearchPokemonTest extends TestCase
     /** @test */
     public function user_can_view_pokemon_in_the_results_using_part_of_the_name()
     {
-        factory(Pokemon::class)->create(['name' => 'Charmander']);
-        factory(Pokemon::class)->create(['name' => 'Charizard']);
-        factory(Pokemon::class)->create(['name' => 'Psyduck']);
-
         $this->visit('/searchPokemon?name=Char')
              ->see('Charmander')
              ->see('Charizard')
@@ -32,10 +28,6 @@ class SearchPokemonTest extends TestCase
     /** @test */
     public function user_can_view_pokemon_in_the_results_using_part_of_the_name_in_lowercase()
     {
-        factory(Pokemon::class)->create(['name' => 'Charmander']);
-        factory(Pokemon::class)->create(['name' => 'Charizard']);
-        factory(Pokemon::class)->create(['name' => 'Psyduck']);
-
         $this->visit('/searchPokemon?name=char')
              ->see('Charmander')
              ->see('Charizard')
@@ -50,10 +42,6 @@ class SearchPokemonTest extends TestCase
     /** @test */
     public function user_can_view_pokemon_in_the_results_using_part_of_the_name_in_uppercase()
     {
-        factory(Pokemon::class)->create(['name' => 'Charmander']);
-        factory(Pokemon::class)->create(['name' => 'Charizard']);
-        factory(Pokemon::class)->create(['name' => 'Psyduck']);
-
         $this->visit('/searchPokemon?name=CHAR')
              ->see('Charmander')
              ->see('Charizard')
@@ -68,10 +56,6 @@ class SearchPokemonTest extends TestCase
     /** @test */
     public function user_can_view_pokemon_in_the_results_using_part_of_the_japanese_name()
     {
-        factory(Pokemon::class)->create(['japanese_name' => 'Hitokage']);
-        factory(Pokemon::class)->create(['japanese_name' => 'Lizardon']);
-        factory(Pokemon::class)->create(['japanese_name' => 'Koduck']);
-
         $this->visit('/searchPokemon?name=Hito')
              ->see('Hitokage')
              ->dontSee('Lizardon');
@@ -88,10 +72,6 @@ class SearchPokemonTest extends TestCase
     /** @test */
     public function user_can_view_pokemon_in_the_results_using_part_of_the_japanese_katakana()
     {
-        factory(Pokemon::class)->create(['japanese_katakana' => 'ヒトカゲ']);
-        factory(Pokemon::class)->create(['japanese_katakana' => 'リザードン']);
-        factory(Pokemon::class)->create(['japanese_katakana' => 'コダック']);
-
         $this->visit('/searchPokemon?name=リザー')
              ->see('リザードン')
              ->dontSee('コダック');
@@ -100,12 +80,6 @@ class SearchPokemonTest extends TestCase
     /** @test */
     public function user_can_view_pokemon_in_the_results_using_the_pokemon_type()
     {
-        factory(Pokemon::class)->create(['name' => 'Charmander', 'type_one' => 'Fire']);
-        factory(Pokemon::class)->create(['name' => 'Charizard', 'type_one' => 'Fire','type_two' => 'Flying']);
-        factory(Pokemon::class)->create(['name' => 'Psyduck', 'type_one' => 'Water']);
-        factory(Pokemon::class)->create(['name' => 'Squirtle', 'type_one' => 'Water']);
-        factory(Pokemon::class)->create(['name' => 'Arceus', 'type_one' => 'Normal']);
-
         $this->visit('/searchPokemon?type=Fire')
              ->see('Charmander')
              ->see('Charizard')
@@ -124,10 +98,6 @@ class SearchPokemonTest extends TestCase
     /** @test */
     public function user_can_view_pokemon_in_the_results_using_the_habitat()
     {
-        factory(Pokemon::class)->create(['name' => 'Pikachu', 'habitat' => 'Forest']);
-        factory(Pokemon::class)->create(['name' => 'Metapod', 'habitat' => 'Forest']);
-        factory(Pokemon::class)->create(['name' => 'Dratini', 'habitat' => 'Fresh Water']);
-
         $this->visit('/searchPokemon?habitat=Forest')
              ->see('Pikachu')
              ->see('Metapod')
@@ -137,13 +107,6 @@ class SearchPokemonTest extends TestCase
     /** @test */
     public function user_can_view_pokemon_in_the_results_using_part_of_the_name_and_type()
     {
-        factory(Pokemon::class)->create(['name' => 'Charmander', 'type_one' => 'Fire']);
-        factory(Pokemon::class)->create(['name' => 'Charizard', 'type_one' => 'Fire','type_two' => 'Flying']);
-        factory(Pokemon::class)->create(['name' => 'Psyduck', 'type_one' => 'Water']);
-        factory(Pokemon::class)->create(['name' => 'Squirtle', 'type_one' => 'Water']);
-        factory(Pokemon::class)->create(['name' => 'Arceus', 'type_one' => 'Normal']);
-        factory(Pokemon::class)->create(['name' => 'Arcanine', 'type_one' => 'Fire']);
-
         $this->visit('/searchPokemon?name=Char&type=Fire')
              ->see('Charmander')
              ->see('Charizard')
@@ -172,13 +135,6 @@ class SearchPokemonTest extends TestCase
     /** @test */
     public function user_can_view_pokemon_in_the_results_using_part_of_the_name_and_habitat()
     {
-        factory(Pokemon::class)->create(['name' => 'Charmander', 'habitat' => 'Mountain']);
-        factory(Pokemon::class)->create(['name' => 'Charizard', 'habitat' => 'Mountain']);
-        factory(Pokemon::class)->create(['name' => 'Psyduck', 'habitat' => 'Fresh Water']);
-        factory(Pokemon::class)->create(['name' => 'Squirtle', 'habitat' => 'Fresh Water']);
-        factory(Pokemon::class)->create(['name' => 'Arceus', 'habitat' => 'Not determined']);
-        factory(Pokemon::class)->create(['name' => 'Arcanine', 'habitat' => 'Meadow']);
-
         $this->visit('/searchPokemon?name=arc&habitat=Meadow')
              ->dontSee('Charmander')
              ->dontSee('Charizard')
@@ -199,13 +155,6 @@ class SearchPokemonTest extends TestCase
     /** @test */
     public function user_can_view_pokemon_in_the_results_using_type_and_habitat()
     {
-        factory(Pokemon::class)->create(['name' => 'Charmander', 'type_one' => 'Fire',  'habitat' => 'Mountain']);
-        factory(Pokemon::class)->create(['name' => 'Charizard',  'type_one' => 'Fire',  'habitat' => 'Mountain', 'type_two' => 'Flying']);
-        factory(Pokemon::class)->create(['name' => 'Psyduck',    'type_one' => 'Water', 'habitat' => 'Fresh Water']);
-        factory(Pokemon::class)->create(['name' => 'Squirtle',   'type_one' => 'Water', 'habitat' => 'Fresh Water']);
-        factory(Pokemon::class)->create(['name' => 'Lapras',     'type_one' => 'Water', 'habitat' => 'Salt Water', 'type_two' => 'Ice']);
-        factory(Pokemon::class)->create(['name' => 'Arcanine',   'type_one' => 'Fire',  'habitat' => 'Meadow']);
-
         $this->visit('/searchPokemon?type=Fire&habitat=Mountain')
              ->see('Charmander')
              ->see('Charizard')
