8aea835c48a7cf10175949a25a572bf83a35ea33
Junior Claudio <juniorclaudiozavaleta@gmail.com>
2017-01-02 18:53:50 +0000

    test trainer cannot favorite a non existent pokemon

--- tests/features/FavoritePokemonTest.php
+++ tests/features/FavoritePokemonTest.php
@@ -35,4 +35,27 @@ class FavoritePokemonTest extends TestCase
             return $pokemon->id == $pikachu->id;
         }));
     }
+
+    /** @test */
+    public function trainer_cannot_favorite_a_non_existent_pokemon()
+    {
+        $ash = factory(User::class, 'ash')->create();
+        $ash_trainer = Trainer::create(['user_id' => $ash->id]);
+
+        $pikachu = factory(Pokemon::class)->make(['name' => 'Pikachu']);
+
+        $this->actingAs($ash, 'trainer');
+
+        $this->assertTrue($ash_trainer->pokemon_favorites->isEmpty());
+
+        $this->call('POST', '/pokemon/add_to_favorites', ['pokemon_id' => $pikachu->id]);
+
+        $this->assertResponseStatus(422);
+
+        $ash_trainer = Trainer::find($ash_trainer->user_id);
+
+        $this->assertFalse($ash_trainer->pokemon_favorites->contains(function ($pokemon) use ($pikachu) {
+            return $pokemon->id == $pikachu->id;
+        }));
+    }
 }
