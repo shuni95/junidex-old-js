4fa4bd49110113979b411ce58ae07eabfea7591e
Junior Claudio <juniorclaudiozavaleta@gmail.com>
2017-01-02 14:26:40 +0000

    test user greater than ten years old

--- tests/features/RegisterTrainerTest.php
+++ tests/features/RegisterTrainerTest.php
@@ -4,6 +4,8 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
+use Carbon\Carbon;
+
 class RegisterTrainerTest extends TestCase
 {
     use WithoutMiddleware;
@@ -66,6 +68,36 @@ class RegisterTrainerTest extends TestCase
         $this->assertSessionHasErrors(['username']);
 
         $this->call('POST', '/trainers/register', array_merge($user, ['username' => 'KalosChampion123']));
+        $this->followRedirects();
+        $this->seePageIs('/trainers/thanks_for_register');
+    }
+
+    /** @test */
+    public function user_only_register_if_age_is_greater_than_ten()
+    {
+        $user = ['name' => 'ash', 'lastname' => 'Ketchum', 'email' => 'ash_champion@test.com', 'username' => 'Kalos Champion', 'password' => '123456', 'confirm_password' => '123456'];
+
+        $this->call('POST', '/trainers/register', array_merge($user, ['birthday' => Carbon::today()]));
+
+        $this->assertSessionHasErrors(['birthday']);
+
+        $this->call('POST', '/trainers/register', array_merge($user, ['birthday' => Carbon::yesterday()]));
+
+        $this->assertSessionHasErrors(['birthday']);
+
+        $this->call('POST', '/trainers/register', array_merge($user, ['birthday' => Carbon::now()]));
+
+        $this->assertSessionHasErrors(['birthday']);
+
+        $this->call('POST', '/trainers/register', array_merge($user, ['birthday' => Carbon::subYears(5)]));
+
+        $this->assertSessionHasErrors(['birthday']);
+
+        $this->call('POST', '/trainers/register', array_merge($user, ['birthday' => Carbon::subYears(9)]));
+
+        $this->assertSessionHasErrors(['birthday']);
+
+        $this->call('POST', '/trainers/register', array_merge($user, ['birthday' => Carbon::subYears(10)]));
         $this->followRedirects();
         $this->seePageIs('/trainers/thanks_for_register');
     }
