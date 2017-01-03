7f560acb3e712e96f122d311adb0b1cbdd75be0f
Junior Claudio <juniorclaudiozavaleta@gmail.com>
2017-01-03 18:53:38 +0000

    register trainer semantic

--- resources/views/app/trainers/register.blade.php
+++ resources/views/app/trainers/register.blade.php
@@ -2,18 +2,54 @@
 <html>
 <head>
   <title>Register trainer</title>
+  <link rel="stylesheet" type="text/css" href="{{ asset('/semantic.min.css') }}">
+  <script src="{{ asset('/semantic.min.js') }}"></script>
 </head>
 <body>
-<form method="POST">
-<input type="text" name="name">
-<input type="text" name="lastname">
-<input type="date" name="birthday">
-<input type="text" name="username">
-<input type="text" name="username">
-<input type="text" name="email">
-<input type="password" name="password">
-<input type="password" name="confirm_password">
-<button type="submit">Register</button>
-</form>
+  <div class="ui container">
+    <form class="ui form" method="POST">
+    {{ csrf_field() }}
+    <h4 class="ui dividing header">Trainer Information</h4>
+    <div class="field">
+      <label>Name</label>
+      <div class="two fields">
+        <div class="field">
+          <input type="text" name="name" placeholder="First Name">
+        </div>
+        <div class="field">
+          <input type="text" name="lastname" placeholder="Last Name">
+        </div>
+      </div>
+    </div>
+    <div class="field">
+      <label>Birthday</label>
+      <div class="field">
+        <input type="date" name="birthday" placeholder="{{ date('Y-m-d') }}">
+      </div>
+    </div>
+    <h4 class="ui dividing header">Credentials</h4>
+    <div class="field">
+      <label>Username</label>
+      <div class="field">
+        <input type="text" name="username" placeholder="Username">
+      </div>
+    </div>
+    <div class="field">
+      <label>Email</label>
+      <input type="text" name="email" placeholder="Email">
+    </div>
+    <div class="fields">
+      <div class="eight wide field">
+        <label>Password</label>
+        <input type="password" name="password">
+      </div>
+      <div class="eight wide field">
+        <label>Confirm Password</label>
+        <input type="password" name="confirm_password">
+      </div>
+    </div>
+    <button class="ui button blue" type="submit">Register</button>
+    </form>
+  </div>
 </body>
 </html>
