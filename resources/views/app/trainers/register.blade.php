73f8c13cf37523acc4e33f26864d951f49aaf6fb
Junior Claudio <juniorclaudiozavaleta@gmail.com>
2017-01-04 14:15:59 +0000

    better presentation register trainer - responsive - button centered

--- resources/views/app/trainers/register.blade.php
+++ resources/views/app/trainers/register.blade.php
@@ -1,55 +1,66 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
   <title>Register trainer</title>
   <link rel="stylesheet" type="text/css" href="{{ asset('/semantic.min.css') }}">
   <script src="{{ asset('/semantic.min.js') }}"></script>
 </head>
 <body>
-  <div class="ui container">
-    <form class="ui form" method="POST">
-    {{ csrf_field() }}
-    <h4 class="ui dividing header">Trainer Information</h4>
-    <div class="field">
-      <label>Name</label>
-      <div class="two fields">
-        <div class="field">
-          <input type="text" name="name" placeholder="First Name">
-        </div>
-        <div class="field">
-          <input type="text" name="lastname" placeholder="Last Name">
-        </div>
-      </div>
-    </div>
-    <div class="field">
-      <label>Birthday</label>
+  <div class="ui container grid">
+    <div class="one wide tablet two wide computer column"></div>
+    <div class="ui form sixteen wide mobile fourteen wide tablet twelve wide computer column">
+      <form method="POST">
+      {{ csrf_field() }}
+      <h4 class="ui dividing header">Trainer Information</h4>
       <div class="field">
-        <input type="date" name="birthday" placeholder="{{ date('Y-m-d') }}">
+        <label>Name</label>
+        <div class="two fields">
+          <div class="field">
+            <input type="text" name="name" placeholder="First Name">
+          </div>
+          <div class="field">
+            <input type="text" name="lastname" placeholder="Last Name">
+          </div>
+        </div>
       </div>
-    </div>
-    <h4 class="ui dividing header">Credentials</h4>
-    <div class="field">
-      <label>Username</label>
       <div class="field">
-        <input type="text" name="username" placeholder="Username">
+        <label>Birthday</label>
+        <div class="field">
+          <input type="date" name="birthday" placeholder="{{ date('Y-m-d') }}">
+        </div>
       </div>
-    </div>
-    <div class="field">
-      <label>Email</label>
-      <input type="text" name="email" placeholder="Email">
-    </div>
-    <div class="fields">
-      <div class="eight wide field">
-        <label>Password</label>
-        <input type="password" name="password">
+      <h4 class="ui dividing header">Credentials</h4>
+      <div class="field">
+        <label>Username</label>
+        <div class="field">
+          <input type="text" name="username" placeholder="Username">
+        </div>
       </div>
-      <div class="eight wide field">
-        <label>Confirm Password</label>
-        <input type="password" name="confirm_password">
+      <div class="field">
+        <label>Email</label>
+        <input type="text" name="email" placeholder="Email">
       </div>
+      <div class="fields">
+        <div class="eight wide field">
+          <label>Password</label>
+          <input type="password" name="password">
+        </div>
+        <div class="eight wide field">
+          <label>Confirm Password</label>
+          <input type="password" name="confirm_password">
+        </div>
+      </div>
+      <div class="field"></div>
+      <div class="ui center aligned grid">
+        <div class="ui sixteen wide mobile six wide computer column">
+          <button class="large ui fluid button blue" type="submit">Register</button>
+        </div>
+      </div>
+      </form>
     </div>
-    <button class="ui button blue" type="submit">Register</button>
-    </form>
+    <div class="one wide tablet two wide computer column"></div>
   </div>
 </body>
 </html>
