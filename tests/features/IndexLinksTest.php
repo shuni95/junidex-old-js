54d6b1dd37fd90c96920c1cbf6c6c33ebd0f6740
Junior Zavaleta <juniorclaudiozavaleta@gmail.com>
2017-01-07 18:47:21 +0000

    trainer can see its username on index when logged test

--- tests/features/IndexLinksTest.php
+++ tests/features/IndexLinksTest.php
@@ -4,13 +4,31 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
+use App\User;
+use App\Trainer;
+
 class IndexLinksTest extends TestCase
 {
+    use DatabaseTransactions;
+
     /** @test */
     public function user_can_move_to_login_trainers_page()
     {
         $this->visit('/')
              ->click('Login')
              ->seePageIs('/trainers/login');
+    }
+
+    /** @test */
+    public function trainer_can_see_its_username_instead_of_login_button()
+    {
+        $ash = factory(User::class, 'ash')->create();
+        Trainer::create(['user_id' => $ash->id]);
+
+        $this->actingAs($ash, 'trainer');
+
+        $this->visit('/')
+             ->see('KalosChampion')
+             ->dontSee('Login');
     }
 }
