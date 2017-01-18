8340a7d8dd0d050e65d98ab171e877ce4b141784
Junior Claudio <juniorclaudiozavaleta@gmail.com>
2017-01-18 21:36:50 +0000

    see pokemon listing on admin test

---
+++ tests/features/admin/SeePokemonListingAdminTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+use App\Pokemon;
+use App\Trainer;
+use App\User;
+
+class SeePokemonListingAdminTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function admin_can_see_pokemon_listing_with_quantity_of_favs_and_some_attributes()
+    {
+        $charmander = factory(Pokemon::class)->create(['name' => 'Charmander']);
+        $charmeleon = factory(Pokemon::class)->create(['name' => 'Charmeleon']);
+        $charizard  = factory(Pokemon::class)->create(['name' => 'Charizard']);
+
+        $ash   = factory(Trainer::class, 'ash')->create();
+        $alain = factory(Trainer::class, 'alain')->create();
+        $gary_user  = factory(User::class)->create();
+        $gary  = Trainer::create(['user_id' => $gary_user->id]);
+
+        $ash->pokemon_favorites()->attach([$charmander->id, $charmeleon->id, $charizard->id]);
+        $alain->pokemon_favorites()->attach([$charmander->id, $charmeleon->id]);
+        $gary->pokemon_favorites()->attach([$charmander->id]);
+
+        $this->visit('/awesome/pokemon/index');
+
+        $this->see('Charmander')
+             ->see('Charmeleon')
+             ->see('Charizard')
+             ->see(1)
+             ->see(2)
+             ->see(3);
+    }
+}
