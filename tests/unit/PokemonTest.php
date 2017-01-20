a1a8054816ea6c34e8084750bb0fed1e98d63f54
Junior Claudio <juniorclaudiozavaleta@gmail.com>
2017-01-20 13:38:22 +0000

    test order desc pokemon by favs

--- tests/unit/PokemonTest.php
+++ tests/unit/PokemonTest.php
@@ -33,4 +33,26 @@ class PokemonTest extends TestCase
         $this->assertTrue($pokemons->first()->id == $charizard->id);
         $this->assertTrue($pokemons->last()->id  == $charmander->id);
     }
+
+    /** @test */
+    function order_desc_by_num_favs()
+    {
+        $charmeleon = factory(Pokemon::class)->create(['name' => 'Charmeleon']);
+        $charizard  = factory(Pokemon::class)->create(['name' => 'Charizard']);
+        $charmander = factory(Pokemon::class)->create(['name' => 'Charmander']);
+
+        $ash   = factory(Trainer::class, 'ash')->create();
+        $alain = factory(Trainer::class, 'alain')->create();
+        $gary_user  = factory(User::class)->create();
+        $gary  = Trainer::create(['user_id' => $gary_user->id]);
+
+        $ash->pokemon_favorites()->attach([$charmander->id, $charmeleon->id, $charizard->id]);
+        $alain->pokemon_favorites()->attach([$charmander->id, $charmeleon->id]);
+        $gary->pokemon_favorites()->attach([$charmander->id]);
+
+        $pokemons = Pokemon::orderByFavs('desc')->get();
+
+        $this->assertTrue($pokemons->first()->id == $charmander->id);
+        $this->assertTrue($pokemons->last()->id  == $charizard->id);
+    }
 }
