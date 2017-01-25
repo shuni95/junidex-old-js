9292d98a683b32f59a268d50dfa1ea6fb5a503b6
Junior Claudio <junior.claudio@tektonlabs.com>
2017-01-25 19:28:55 +0000

    other tests updated

--- tests/Features/SearchPokemonTest.php
+++ tests/Features/SearchPokemonTest.php
@@ -1,5 +1,9 @@
 <?php
 
+namespace TestZone\Features;
+
+use TestZone\TestCase;
+
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
