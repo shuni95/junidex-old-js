f8bcf7d6965a75d33d7761beaeddef409c722318
Junior Claudio <junior.claudio@tektonlabs.com>
2017-01-25 19:25:55 +0000

    Api test updated

--- tests/Features/Api/ListAllPokemonApiTest.php
+++ tests/Features/Api/ListAllPokemonApiTest.php
@@ -1,5 +1,9 @@
 <?php
 
+namespace TestZone\Features\Api;
+
+use TestZone\TestCase;
+
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
